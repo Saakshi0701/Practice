741bacd4af1f6e559ee3ed8e540aeba28a943b7c
Saakshi0701 <Sakshi.Dhoke@cognizant.com>
2024-09-30 14:52:27 +0530

    DbContext

---
+++ Data/PracticeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticeProject.Model.Domain;
+
+namespace PracticeProject.Data
+{
+    public class PracticeDbContext : DbContext
+    {
+        public PracticeDbContext(DbContextOptions<PracticeDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<BlogImage> BlogImages { get; set; }
+
+    }
+}
